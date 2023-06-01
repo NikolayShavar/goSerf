2c06e6aea00be7eae0e3958cf58439b10beffbfb
NikolayShavar <fargon45821@gmail.com>
2023-06-01 16:16:24 +0300

    01.06.2023-1

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ $(function(){
 $('.header__slider').slick({
     infinite: true,
     fade: true,
+    prevArrow:'<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg"/>',
+    nextArrow:'<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg"/>'
 });
 
 });
