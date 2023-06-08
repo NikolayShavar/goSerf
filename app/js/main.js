2b092b58e8802c498d8f57b70969ac0746c2b674
NikolayShavar <fargon45821@gmail.com>
2023-06-08 17:31:13 +0300

    465

--- app/js/main.js
+++ app/js/main.js
@@ -12,9 +12,20 @@ $(function () {
   $(".slider-dots").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-	arrows: false,
-	dots: false,
+    arrows: false,
+    dots: false,
     asNavFor: ".header__slider",
-	focusOnSelect: true,
+    focusOnSelect: true,
+  });
+
+  $(".serf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    focusOnSelect: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg"/>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg"/>',
   });
 });
