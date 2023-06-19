37fab209dc53e917376648a2002de8e61ce03a3c
NikolayShavar <fargon45821@gmail.com>
2023-06-19 16:53:39 +0300

    sUrffix

--- app/js/main.js
+++ app/js/main.js
@@ -18,7 +18,7 @@ $(function () {
     focusOnSelect: true,
   });
 
-  $(".serf-slider").slick({
+  $(".surf-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     dots: false,
@@ -27,14 +27,14 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg"/>',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg"/>',
-    asNavFor:'.slider-map',
-    });
+    asNavFor: ".slider-map",
+  });
 
   $(".slider-map").slick({
     slidesToShow: 8,
     slidesToScroll: 1,
     arrows: false,
     focusOnSelect: true,
-    asNavFor:'.serf-slider',
+    asNavFor: ".surf-slider",
   });
 });
