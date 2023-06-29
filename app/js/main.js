a2110dd9044a5a44dc2a6525005fb884b0742099
NikolayShavar <fargon45821@gmail.com>
2023-06-29 19:24:09 +0300

    463

--- app/js/main.js
+++ app/js/main.js
@@ -38,7 +38,7 @@ $(function () {
     asNavFor: ".surf-slider",
   });
 
-  $('.holder__slider').slick({
+  $(".holder__slider").slick({
     infinite: true,
     fade: true,
     prevArrow:
@@ -47,16 +47,18 @@ $(function () {
       '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg"/>',
   });
 
- $('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
-  $('.quantity').each(function() {
+  $(
+    '<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg"></div><div class="quantity-button quantity-down"><img src="images/minus.svg"></div></div>'
+  ).insertAfter(".quantity input");
+  $(".quantity").each(function () {
     var spinner = $(this),
       input = spinner.find('input[type="number"]'),
-      btnUp = spinner.find('.quantity-up'),
-      btnDown = spinner.find('.quantity-down'),
-      min = input.attr('min'),
-      max = input.attr('max');
+      btnUp = spinner.find(".quantity-up"),
+      btnDown = spinner.find(".quantity-down"),
+      min = input.attr("min"),
+      max = input.attr("max");
 
-    btnUp.click(function() {
+    btnUp.click(function () {
       var oldValue = parseFloat(input.val());
       if (oldValue >= max) {
         var newVal = oldValue;
@@ -67,7 +69,7 @@ $(function () {
       spinner.find("input").trigger("change");
     });
 
-    btnDown.click(function() {
+    btnDown.click(function () {
       var oldValue = parseFloat(input.val());
       if (oldValue <= min) {
         var newVal = oldValue;
@@ -77,7 +79,18 @@ $(function () {
       spinner.find("input").val(newVal);
       spinner.find("input").trigger("change");
     });
-
   });
 
+  $(".quantity-button").on("click", function (){
+    let summ =
+      $(".nights").val() * $(".summ").data("nights") +
+      ($(".guests").val() - 1) * $(".summ").data("guests");
+      $(".summ").html("$" + summ);
+  });
+
+  let summ =
+    $(".nights").val() * $(".summ").data("nights") +
+    ($(".guests").val() - 1) * $(".summ").data("guests");
+
+  $(".summ").html("$" + summ);
 });
