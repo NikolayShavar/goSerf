bcc349331ea659aeea27610cebeb557d8e15affa
NikolayShavar <fargon45821@gmail.com>
2023-07-13 17:46:01 +0300

    472

--- app/js/main.js
+++ app/js/main.js
@@ -97,4 +97,7 @@ $(function () {
   $(".surfboard-box__circle").on("click", function () {
     $(this).toggleClass("active");
   });
+  $('.menu-btn').on('click', function(){
+$('.menu').toggleClass('active');
+  });
 });
